286b6be77d80602ead57f19f233cbfe6d46d6342
agivdel <agph@yandex.ru>
2021-01-06 13:40:29 +0300

    correct all tests

--- src/test/java/agivdel/library/MultiDeliveryDeskTest.java
+++ src/test/java/agivdel/library/MultiDeliveryDeskTest.java
@@ -3,12 +3,8 @@ package agivdel.library;
 import org.junit.*;
 import org.junit.rules.ExpectedException;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MultiDeliveryDeskTest {
     MultiDeliveryDesk mdd;
-    List<Book> bookList;
 
     Book book0 = new Book();
     Book book1 = new Book();
@@ -16,13 +12,19 @@ public class MultiDeliveryDeskTest {
     Book book3 = new Book();
     String student1 = "Emily";
     String student2 = "Olivia";
-    String student3 = "Blunt"; // не используется
+    String student3 = "Blunt";
 
     @Rule
     public ExpectedException expectedEx = ExpectedException.none();
 
+    @Before
+    public void beforeTest() {
+        mdd = new MultiDeliveryDesk();
+    }
+
     @Test
-    public void multiDeliveryDeskTest() { // это ваше чо? пустой метод
+    public void multiDeliveryDeskTest() {
+        Assert.assertNotNull(mdd);
     }
 
     @Test
@@ -45,36 +47,39 @@ public class MultiDeliveryDeskTest {
     }
 
     @Test
-    public void addNullBookTitleTest() { // addNewBookConvertNullTitle
+    public void addNullBookNullTitleConvertTest() {
         mdd.addNewBook(new Book());
         Assert.assertEquals("null", mdd.findAvailableBooks().get(0).title);
     }
 
     @Test
-    public void addEmptyBookTitleTest() { // addNewBookNameTrimmedTest
+    public void addNewBookEmptyTitleTrimTest() {
         book0.title = "  ";
         mdd.addNewBook(book0);
         Assert.assertEquals("", mdd.findAvailableBooks().get(0).title);
     }
 
     @Test
-    public void addNotUniqueTitleTest() { // addNewBookItIsPossibleToAddBooksWithNonUniqueNamesTest
+    public void addNewBookNotUniqueTitlesPossibleTest() {
+        Assert.assertEquals(0, mdd.findAvailableBooks().size());
         book0.id = 1;
         mdd.addNewBook(book0);
         book1.id = 2;
         mdd.addNewBook(book1);
         book2.id = 3;
         mdd.addNewBook(book2);
-
-        //ни единого ассерта
+        Assert.assertEquals(3, mdd.findAvailableBooks().size());
     }
 
     @Test
     public void addCorrectBookTest() {
         addSomeBooks(book0, book1, book2);
-        //какие-то выборочные ассерты, почему бы не проверитиь вообще все?
 
         Assert.assertEquals(0, mdd.findAvailableBooks().get(0).id);
+        Assert.assertEquals("book0", mdd.findAvailableBooks().get(0).title);
+        Assert.assertEquals(1, mdd.findAvailableBooks().get(1).id);
+        Assert.assertEquals("book1", mdd.findAvailableBooks().get(1).title);
+        Assert.assertEquals(2, mdd.findAvailableBooks().get(2).id);
         Assert.assertEquals("book2", mdd.findAvailableBooks().get(2).title);
     }
 
@@ -111,7 +116,7 @@ public class MultiDeliveryDeskTest {
         addSomeBooks(book0, book1, book2);
         book3.id = 1;
         book3.title = "book0";
-        mdd.borrowBook(book3, student1);
+        mdd.borrowBook(book3, student2);
         Assert.assertEquals(2, mdd.findAvailableBooks().size());
         Assert.assertEquals(0, mdd.findAvailableBooks().get(0).id);
         Assert.assertEquals(2, mdd.findAvailableBooks().get(1).id);
@@ -152,13 +157,15 @@ public class MultiDeliveryDeskTest {
     }
 
     @Test
-    public void returnByAnotherStudent() {
+    public void returnByAnotherStudentTest() {
         book1.id = 1;
         book1.title = "book1";
         exPrepare(String.format(Constants.mustBeReturned, book1.id, book1.title, student1) + Constants.notReturned);
         mdd.addNewBook(book1);
         mdd.borrowBook(book1, student1);
-        mdd.returnBook(book1, student2);
+        Assert.assertEquals(0, mdd.findAvailableBooks().size());
+        mdd.returnBook(book1, student3);
+        Assert.assertEquals(0, mdd.findAvailableBooks().size());
     }
 
     @Test
@@ -171,31 +178,19 @@ public class MultiDeliveryDeskTest {
 
     @Test
     public void findAvailableBooksTest() {
-        addSomeBooks(book0, book1);
+        addSomeBooks(2);
         Assert.assertEquals(2, mdd.findAvailableBooks().size());
     }
 
     @Test
     public void externalRewritingOfAvailableBooksTest() {
-        addSomeBooks(book0);
+        addSomeBooks(1);
         Book book = mdd.findAvailableBooks().get(0);
         book.id = 100;
         book.title = "hundred";
         Book newBook = mdd.findAvailableBooks().get(0);
         Assert.assertNotEquals(book.id, newBook.id);
         Assert.assertEquals(0, newBook.id);
-    }
-
-    @Before
-    public void beforeTest() {
-        mdd = new MultiDeliveryDesk();
-        bookList = new ArrayList<>(); // не используется
-    }
-
-    @After
-    public void clear() { //вообще не нужно
-        mdd = null;
-        bookList = null;
     }
 
     private void addSomeBooks(Book...books) {
@@ -206,7 +201,7 @@ public class MultiDeliveryDeskTest {
         }
     }
 
-    private void addSomeBooks(int number) { // не используется
+    private void addSomeBooks(int number) {
         Book book = new Book();
         for (int i = 0; i < number; i++) {
             book.id = i;
@@ -214,5 +209,4 @@ public class MultiDeliveryDeskTest {
             mdd.addNewBook(book);
         }
     }
-
 }
